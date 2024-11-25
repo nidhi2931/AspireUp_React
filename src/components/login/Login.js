4cdbeb1e0c356c2720a34e0d0d2cc1bdfe77ee91
nidhi2931 <imnidhi.gautam8690@gmail.com>
2024-11-25 22:01:43 +0530

    first commit

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,10 +1,9 @@
 
 import React,{ useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { BrowserRouter, Link } from 'react-router-dom';
+import {Container,Row,Col,Form,Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const Login=()=>{
+const Login =()=> {
     const[emailInput,setEmailInput]=useState('');
     const[passwordInput,setPasswordInput]=useState('');
     
@@ -23,35 +22,40 @@ const Login=()=>{
     }
 
     return(
-        <BrowserRouter>
-        <div className='container mt-4'>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId='emailInput'>
-                    <Form.Label>Email</Form.Label>
-                        <Form.Control
-                        type="email"
-                        placeholder="Enter Email"
-                        value={emailInput}
-                        onChange={handleEmailInput}
-                        />
-                </Form.Group>
-                <Form.Group controlId="passwordInput">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                    type="password"
-                    placeholder="Enter Password"
-                    value={passwordInput}
-                    onChange={handlePasswordInput}
-                    />
-                </Form.Group>
-                <Button type ="submit" variant="primary" className='mt-3'>Submit</Button>
-            </Form>
-            <p>Don't you have an account?
-                <Link to="/signup" className="text-primary">Signup here</Link>
-            </p>
-        </div>
-        </BrowserRouter>
+       
+        <Container className='mt-5'>
+            <Row className='justify-content-md-center'>
+                <Col md={4}>
+                    <h2 className='text-center mb-4'>Login</h2>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId='emailInput'>
+                            <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                type="email"
+                                placeholder="Enter Email"
+                                value={emailInput}
+                                onChange={handleEmailInput}
+                                />
+                        </Form.Group>
+                        <Form.Group controlId="passwordInput">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                            type="password"
+                            placeholder="Enter Password"
+                            value={passwordInput}
+                            onChange={handlePasswordInput}
+                            />
+                        </Form.Group>
+                        <Button type ="submit" variant="primary" className='mt-3'>Submit</Button>
+                    </Form>
+                    <p>Don't you have an account?{' '}
+                        <Link to="/signup" className="text-primary">Sign Up</Link>
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+        
     )
 }
 
-export default Login
+export default Login;
